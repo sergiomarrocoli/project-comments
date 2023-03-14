debe0871e747b1f20a9631a1f851272161205861
sergiomarrocoli <s.marrocoli@gmail.com>
2023-03-14 21:16:21 +0000

    feat: styled Project index

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,15 @@ module ProjectsHelper
   def project_status_options
     Project.statuses.keys.map { |key| [key, key] }
   end
+
+  def project_pill_colour(status)
+    case status
+    when 'to do'
+      'green-500'
+    when 'in progress'
+      'red-500'
+    when 'complete'
+      'black'
+    end
+  end
 end
