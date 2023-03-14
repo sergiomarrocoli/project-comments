81cc2c2674eaaf43ce3cb92f02946c685badbeb9
sergiomarrocoli <s.marrocoli@gmail.com>
2023-03-14 21:34:49 +0000

    feat: updated seeds to include colleague conversation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 users = [
-  { email: 'user_1@example.com', username: 'User 1', password: 'password', password_confirmation: 'password' },
+  { email: 'sergio@example.com', username: 'Sergio', password: 'password', password_confirmation: 'password' },
   { email: 'user_2@example.com', username: 'User 2', password: 'password', password_confirmation: 'password' }
 ]
 User.create!(users)
 
 projects = [
-  { title: 'Project 1', status: 'to do' },
+  { title: 'Project Comments App', status: 'to do' },
   { title: 'Project 2', status: 'in progress' },
   { title: 'Project 3', status: 'complete' }
 ]
@@ -19,8 +19,12 @@ user_2 = User.second
 project = Project.first
 
 comments = [
-  { body: 'Comment 1', user: user_1, project:},
-  { body: 'Comment 2', user: user_2, project:},
-  { body: 'Comment 3', user: user_1, project:}
+  { body: "I'm assigned this ticker and have a few questions. Do we know what kind of authentication is required?", user: user_1, project:},
+  { body: "Standard Devise setup for this, but for now don't allow sign up.
+          Until we implement permissions we only want to allow users who we know should be commenting on these projects", user: user_2, project:},
+  { body: 'Do we know what project statuses I need to implement?', user: user_1, project:},
+  { body: "Eventually there will be a UI for users to create custom statuses. For testing purposes just add 'to do', 'in progress' and 'complete'", user: user_2, project:},
+  { body: 'Do we have designs for this?', user: user_1, project:},
+  { body: 'No, and the schedule is pretty tight. I know your css is terrible, just do what you can.', user: user_1, project:}
 ]
 Comment.create!(comments)
